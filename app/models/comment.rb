a5ce8453914e8adb6d9fe027ef7bcdb39c081d71
Rmiverson <Rmiverson99@gmail.com>
2021-02-26 10:24:05 -0800

    added dependent destroy to applicable models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-   belongs_to :user
-   belongs_to :post
+   belongs_to :user, :dependent :destroy
+   belongs_to :post, :dependent :destroy
 
    has_many :comment_likes
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
    has_many :comments
    has_many :post_likes
 
-   belongs_to :user
+   belongs_to :user, :dependent :destroy
 end
