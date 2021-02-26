6833f678cc222d7f72766a38145c4162891c9e83
Rmiverson <Rmiverson99@gmail.com>
2021-02-25 18:04:31 -0800

    setup selfjoin for following peeps

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
    has_many :post_likes
    has_many :comment_likes
 
-   has_many :followings, foreign_key: "follower_id", dependent: :destroy
-   has_many :followed_users, through: :followings, source: :followed
-
    has_many :posts, through: :post_likes
    has_many :comments, through: :comment_likes
 
-   # belongs_to :associated_object, polymorphic: true
+   has_many :followers, foreign_key: :follower_id, class_name: "Relationship"
+   has_many :followed, through: :followers
+
+   has_many :followed, foreign_key: :followed_id, class_name: "Relationship"
+   has_many :follwers, through: :followed
 end
