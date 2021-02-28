23a220d782c9dfc7670d2f800491349d94c0082d
Rmiverson <Rmiverson99@gmail.com>
2021-02-27 23:25:30 -0800

    done did the login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show, :create]
-  resources :posts, only: [:index, :show]
-  resources :comments, only: [:show]
+  namespace :api do
+    namespace :v1 do
+        resources :users, only: [:index, :show, :create]
+        resources :posts, only: [:index, :show]
+        resources :comments, only: [:show]
 
-  post "/login", to: "users#login"
-  get "/auto_login", to: "users#auto_login"
+        post '/sign_up', to: 'users#create'
+        post "/login", to: "auth#create"
+        get '/persist', to: 'auth#show'
+    end
+  end
+
+  # get "/auto_login", to: "users#auto_login"
 end
