dcc2986775a08d5ea24068f95351c245adb139aa
Rmiverson <Rmiverson99@gmail.com>
2021-03-02 16:30:02 -0800

    updated seeds n things

--- db/schema.rb
+++ db/schema.rb
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2021_02_26_020013) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.text "user_desc"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
