c6b4e4f472bbd477750c47369d0d176d57b31ea7
Rmiverson <Rmiverson99@gmail.com>
2021-03-03 22:22:59 -0800

    added ability to follow other users

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,4 @@
 class Relationship < ApplicationRecord
-   belongs_to :follower, class_name: "User", dependent: :destroy
-   belongs_to :followee, class_name: "User", dependent: :destroy
+   belongs_to :follower, class_name: "User"
+   belongs_to :followee, class_name: "User"
 end
