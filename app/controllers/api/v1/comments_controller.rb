c5604cbf53d1b51a86e9b15f7a9e8d19175ffb50
Rmiverson <Rmiverson99@gmail.com>
2021-03-05 11:52:07 -0800

    added create methods to comment controller and routes

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,7 +1,24 @@
 class Api::V1::CommentsController < ApplicationController
+   def create
+      comment = Comment.create(comment_params)
+      
+      if comment.valid?
+         render json: CommentSerializer.new(comment).serialized_json
+      else
+         render json: {error: "Invalid inputs for new comment"}
+      end
+   end
+
    def show
       comment = Comment.find(params[:id])
 
       render json: CommentSerializer.new(comment).serialized_json
    end
+
+   private
+
+   def comment_params
+      params.require(:comment).permit(:id, :user_id, :post_id, :content)
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
         resources :users, only: [:index, :show, :create]
         resources :posts, only: [:index, :show, :create]
-        resources :comments, only: [:show]
+        resources :comments, only: [:show, :create]
 
         post '/sign_up', to: 'users#create'
         post "/login", to: "auth#create"
