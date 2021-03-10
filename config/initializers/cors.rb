2c81204d3fe1f12317c8aaf355343b210a135069
Rmiverson <Rmiverson99@gmail.com>
2021-03-10 10:40:23 -0800

    fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://rmiverson.github.io/goodbread-frontend/' #change when pushing to heroku
 
-    resource '/api/v1/*',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
