4aece90dc96ee379f308bc0a21620cc2ac5aad9f
Rmiverson <Rmiverson99@gmail.com>
2021-03-10 10:10:51 -0800

    setup for heroku push

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' #change when pushing to heroku
+    origins 'https://rmiverson.github.io/goodbread-frontend/' #change when pushing to heroku
 
-    resource '*',
+    resource '/api/v1/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
