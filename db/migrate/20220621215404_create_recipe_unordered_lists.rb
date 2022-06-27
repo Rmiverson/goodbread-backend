ec2ee1c0f3a23baf3ee9744412e7e7d69a18c401
Rmiverson <Rmiverson99@gmail.com>
2022-06-27 10:34:17 -0700

    moved new controllers into the proper api folders, added migration values

--- db/migrate/20220621215404_create_recipe_unordered_lists.rb
+++ db/migrate/20220621215404_create_recipe_unordered_lists.rb
@@ -1,7 +1,8 @@
 class CreateRecipeUnorderedLists < ActiveRecord::Migration[6.1]
   def change
     create_table :recipe_unordered_lists do |t|
-
+      t.string :header
+      t.
       t.timestamps
     end
   end
--- db/migrate/20220621215411_create_recipe_texts.rb
+++ db/migrate/20220621215411_create_recipe_texts.rb
@@ -1,7 +1,8 @@
 class CreateRecipeTexts < ActiveRecord::Migration[6.1]
   def change
     create_table :recipe_texts do |t|
-
+      t.string :header
+      t.text :content
       t.timestamps
     end
   end
